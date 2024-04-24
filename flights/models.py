6cbaf5977c677d05b47f75d96fd4db9577963d76
ErickGBR <erickburgos1519@gmail.com>
2024-04-23 18:38:26 -0600

    feat: create model of flight proyect

--- flights/models.py
+++ flights/models.py
@@ -5,3 +5,5 @@ class Flight(models.Model):
     origin = models.CharField(max_length=64)
     destination = models.CharField(max_length=64)
     duration = models.IntegerField(default=0)
+    def __str__(self):
+        return f"{self.id}: {self.origin} {self.destination}"
